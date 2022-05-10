3d002e28697405a08c5176ddb322a84b5ace4b90
tyrion <tyrion@cht.com.tw>
2022-05-10 15:46:10 +0800

    compute

--- lab6_vue3_two_way_binding/app.js
+++ lab6_vue3_two_way_binding/app.js
@@ -1,8 +1,23 @@
 const app = Vue.createApp({
-    data() { return { issue: "" } },
+    data() {
+        return {
+            issue: "",
+            counter: 0
+        }
+    },
     methods: {
+        outputIssue() {
+            console.log(`output issue is called:${this.issue}`)
+            if (this.issue === "") {
+                return ""
+            }
+            return `[!!!]${this.issue}`
+        },
         resetIssue() {
             this.issue = "";
+        },
+        increase() {
+            this.counter += 1;
         }
     }
 })
