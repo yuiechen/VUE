c8f3f5b75fb66bddd85ef840e01f1f307f9c2a7a
tyrion <tyrion@cht.com.tw>
2022-05-10 14:39:39 +0800

    v-on binding

--- lab5_vue3_callback/app.js
+++ lab5_vue3_callback/app.js
@@ -1,17 +1,41 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter1: 0
+            counter1: 0,
+            todo: '',
+            urgent: '',
+            committedUrgent: '',
+            counter2: 888
         }
     },
     methods: {
+        increase2_4(step) {
+            this.counter2 += step
+        },
+        decrease2_4(step) { this.counter2 -= step },
+        setUrgent(ev) {
+            this.urgent = ev.target.value
+        },
+        commitUrgent() {
+            this.committedUrgent = this.urgent
+        },
+        submitForm(ev) {
+            ev.preventDefault()
+            alert('已經提交了~~~')
+        },
+        submitForm2() {
+            alert('還是提交了321')
+        },
+        setToDo(event, message) {
+            this.todo = `${message} ${event.target.value}`
+        },
         increase1() {
             this.counter1 += 1;
         },
         decrease1() {
             this.counter1 -= 1;
         },
-        increase2(step){
+        increase2(step) {
             this.counter1 += step;
         },
         decrease2(step) {
