efb2cdc0ad7f0f170b1164465896bb60605912d6
tyrion <tyrion@cht.com.tw>
2022-05-10 16:36:29 +0800

    縮寫

--- lab6_vue3_two_way_binding/app.js
+++ lab6_vue3_two_way_binding/app.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             issue: "",
-            counter: 0
+            counter: 0,
+            outputIssue: "",
+            issue2: "hello",
+            issue3: "world"
         }
     },
     methods: {
@@ -14,15 +17,31 @@ const app = Vue.createApp({
             this.counter += 1;
         }
     },
-    computed: {
-        outputIssue() {
-            console.log(`output issue is called:${this.issue}`)
+    watch: {
+        issue(value, oldValue) {
+            console.log("issue的值改變了, 需要更新")
             if (this.issue === "") {
                 return ""
             }
-            let prefix = "!".repeat(this.counter)
-            return `[${prefix}]${this.issue}`
+            this.outputIssue = `[!!]從${oldValue}到${value}`
         },
+        counter(value) {
+            console.log("counter now change to:", value)
+            if (value > 10) {
+                this.counter = 0
+                console.log("counter reset")
+            }
+        }
+    },
+    computed: {
+        // outputIssue() {
+        //     console.log(`output issue is called:${this.issue}`)
+        //     if (this.issue === "") {
+        //         return ""
+        //     }
+        //     let prefix = "!".repeat(this.counter)
+        //     return `[${prefix}]${this.issue}`
+        // },
     }
 })
 app.mount("#myapp")
